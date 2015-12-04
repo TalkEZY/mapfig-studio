ac186fa55a56ebf2a8c53fb2ce088d0d0e0df150
DavidGhedini <david@davidghedini.com>
2015-12-04 02:24:41 +0200

    edit profile

---
+++ app/edit_profile.php
@@ -0,0 +1,240 @@
+<?PHP
+	require_once(dirname(__FILE__)."/../include/master.inc.php");
+	if(!isLogin()){
+		redirect("login.php");
+	}
+	
+	$error   = "";
+	$success = "";
+	if(strlen($_SESSION['response']['edit_profile']['error']) > 0){
+		$error = '<div ng-show="authMsg" class="alert alert-danger text-center ng-binding">'.$_SESSION['response']['edit_profile']['error'].'</div>';
+	}
+	else if(strlen($_SESSION['response']['edit_profile']['success']) > 0){
+		$success = '<div ng-show="authMsg" class="alert alert-success text-center ng-binding">'.$_SESSION['response']['edit_profile']['success'].'</div>';
+	}
+	unset($_SESSION['response']['edit_profile']['error']);
+	unset($_SESSION['response']['edit_profile']['success']);
+	
+	$PAGE = "EDIT_PROFILE";
+	$user = $_SESSION['user'];
+	$total_maps = getMapsCount($user['id']);
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+   <meta charset="utf-8">
+   <title>Edit/Update Profile - <?=$user['firstname'].' '.$user['lastname']?></title>
+   
+   <?PHP require_once(dirname(__FILE__)."/../include/header.tpl.php"); ?>
+   
+   
+   <!-- =============== VENDOR STYLES ===============-->
+   <!-- FONT AWESOME-->
+   <link rel="stylesheet" href="../vendor/fontawesome/css/font-awesome.min.css">
+   <!-- SIMPLE LINE ICONS-->
+   <link rel="stylesheet" href="../vendor/simple-line-icons/css/simple-line-icons.css">
+   <!-- ANIMATE.CSS-->
+   <link rel="stylesheet" href="../vendor/animate.css/animate.min.css">
+   <!-- WHIRL (spinners)-->
+   <link rel="stylesheet" href="../vendor/whirl/dist/whirl.css">
+   <!-- =============== PAGE VENDOR STYLES ===============-->
+   <!-- WEATHER ICONS-->
+   <link rel="stylesheet" href="../vendor/weather-icons/css/weather-icons.min.css">
+   <!-- =============== APP STYLES ===============-->
+   <link rel="stylesheet" href="css/app.css" id="maincss">
+</head>
+
+<body class="aside-collapsed">
+   <div class="wrapper">
+      <!-- top navbar-->
+      <header class="topnavbar-wrapper">
+         <!-- START Top Navbar-->
+         <nav role="navigation" class="navbar topnavbar">
+            <!-- START navbar header-->
+            <div class="navbar-header">
+               <a href="#/" class="navbar-brand">
+                  <div class="brand-logo">
+                     <img src="img/logo.png" alt="App Logo" class="img-responsive">
+                  </div>
+                  <div class="brand-logo-collapsed">
+                     <img src="img/logo-single.png" alt="App Logo" class="img-responsive">
+                  </div>
+               </a>
+            </div>
+            <!-- END navbar header-->
+            <!-- START Nav wrapper-->
+            <div class="nav-wrapper">
+               <!-- START Left navbar-->
+               <ul class="nav navbar-nav">
+                  <li>
+                     <!-- Button used to collapse the left sidebar. Only visible on tablet and desktops-->
+                     <a href="#" data-toggle-state="aside-collapsed" class="hidden-xs">
+                        <em class="fa fa-navicon"></em>
+                     </a>
+                     <!-- Button to show/hide the sidebar on mobile. Visible on mobile only.-->
+                     <a href="#" data-toggle-state="aside-toggled" data-no-persist="true" class="visible-xs sidebar-toggle">
+                        <em class="fa fa-navicon"></em>
+                     </a>
+                  </li>
+                  <!-- START User avatar toggle-->
+                  <li>
+                     <!-- Button used to collapse the left sidebar. Only visible on tablet and desktops-->
+                     <a href="#user-block" data-toggle="collapse">
+                        <em class="icon-user"></em>
+                     </a>
+                  </li>
+                  <!-- END User avatar toggle-->
+                  <!-- START lock screen-->
+                  <li>
+                     <a href="lock.php" title="Lock screen">
+                        <em class="icon-lock"></em>
+                     </a>
+                  </li>
+                  <!-- END lock screen-->
+               </ul>
+               <!-- END Left navbar-->
+               <!-- START Right Navbar-->
+               <ul class="nav navbar-nav navbar-right">
+				  <!-- Search icon-->
+				  <li>
+                     <a href="#" data-search-open="">
+                        <em class="icon-magnifier"></em>
+                     </a>
+                  </li>
+                  <li>
+                     <a href="http://docs.mapfig.com/" target="_blank" title="Launch Help Wizard">
+                        <em class="fa fa-question-circle"></em>
+                     </a>
+                  </li>
+                  <!-- Fullscreen (only desktops)-->
+                  <li class="visible-lg">
+                     <a href="#" data-toggle-fullscreen="">
+                        <em class="fa fa-expand"></em>
+                     </a>
+                  </li>
+               </ul>
+               <!-- END Right Navbar-->
+            </div>
+            <!-- END Nav wrapper-->
+			<!-- START Search form-->
+            <form role="search" action="map-search.php" class="navbar-form">
+               <div class="form-group has-feedback">
+                  <input type="text" placeholder="Type Map Title/Name and hit enter ..." name="q" class="form-control">
+                  <div data-search-dismiss="" class="fa fa-times form-control-feedback"></div>
+               </div>
+               <button type="submit" class="hidden btn btn-default">Submit</button>
+            </form>
+            <!-- END Search form-->
+            
+         </nav>
+         <!-- END Top Navbar-->
+      </header>
+      
+	  <?PHP require_once(dirname(__FILE__)."/../include/sidebar.tpl.php") ?>
+	  
+      <!-- Main section-->
+      <section>
+		 <!-- Page content-->
+		 <div class="content-wrapper">
+			<div class="unwrap">
+			   <div style="background:#585858;" class="bg-cover">
+				  <div class="p-xl text-center text-white">
+					 <img src="img/user/09.jpg" alt="Image" class="img-thumbnail img-circle thumb128">
+					 <h3 class="m0"><?=$user['firstname'].' '.$user['lastname']?></h3>
+					 <p>User</p>
+				  </div>
+			   </div>
+			   <div class="text-center bg-gray-dark p-lg mb-xl">
+				  <div class="row row-table">
+					 <div class="col-xs-4 br">
+						<p class="m0">
+						</p>
+					 </div>
+					 <div class="col-xs-4 br">
+						<h3 class="m0"><?=$total_maps?></h3>
+						<p class="m0">Maps</p>
+					 </div>
+					 <div class="col-xs-4">
+					 </div>
+				  </div>
+			   </div>
+			   <div class="p-lg">
+				  <div class="row">
+					 <div class="col-lg-12">
+						<div class="panel panel-default">
+						   <div class="panel-body">
+							  <?=$error.$success?>
+							  <div class="table-responsive">
+								<form role="form" action="processor/edit_profile/index.php" method="post" data-parsley-validate="" novalidate="" class="mb-lg">
+								  <div class="form-group has-feedback">
+								     <label for="editInputFirstName" class="text-muted">First Name</label>
+									 <input id="editInputFirstName" type="text" value="<?=$user['firstname']?>" name="firstname" placeholder="Enter First Name" autocomplete="off" required class="form-control">
+									 <span class="fa fa-user form-control-feedback text-muted"></span>
+								  </div>
+								  <div class="form-group has-feedback">
+									 <label for="editInputLastName" class="text-muted">Last Name</label>
+									 <input id="editInputLastName" type="text" value="<?=$user['lastname']?>" placeholder="Enter Last Name" name="lastname" autocomplete="off" required class="form-control">
+									 <span class="fa fa-user form-control-feedback text-muted"></span>
+								  </div>
+								  <div class="form-group has-feedback">
+									 <label for="editInputEmail1" class="text-muted">Email address</label>
+									 <input id="editInputEmail1" type="email" value="<?=$user['email']?>" placeholder="Enter Email Address" name="email" autocomplete="off" required class="form-control">
+									 <span class="fa fa-envelope form-control-feedback text-muted"></span>
+								  </div>
+								  <button type="submit" name="edit_profile" class="btn btn-block btn-primary mt-lg">Edit Profile</button>
+							  </div>
+						   </div>
+						</div>
+					 </div>
+				  </div>
+			   </div>
+			</div>
+		 </div>
+	  </section>
+      <?PHP require_once(dirname(__FILE__)."/../include/footer.tpl.php"); ?>
+   </div>
+   <!-- =============== VENDOR SCRIPTS ===============-->
+   <!-- MODERNIZR-->
+   <script src="../vendor/modernizr/modernizr.js"></script>
+   <!-- BOOTSTRAP-->
+   <script src="../vendor/bootstrap/dist/js/bootstrap.js"></script>
+   <!-- STORAGE API-->
+   <script src="../vendor/jQuery-Storage-API/jquery.storageapi.js"></script>
+   <!-- JQUERY EASING-->
+   <script src="../vendor/jquery.easing/js/jquery.easing.js"></script>
+   <!-- ANIMO-->
+   <script src="../vendor/animo.js/animo.js"></script>
+   <!-- SLIMSCROLL-->
+   <script src="../vendor/slimScroll/jquery.slimscroll.min.js"></script>
+   <!-- SCREENFULL-->
+   <script src="../vendor/screenfull/dist/screenfull.min.js"></script>
+   <!-- LOCALIZE-->
+   <script src="../vendor/jquery-localize-i18n/dist/jquery.localize.js"></script>
+   <!-- RTL demo-->
+   <script src="js/demo/demo-rtl.js"></script>
+   <!-- =============== PAGE VENDOR SCRIPTS ===============-->
+   <!-- SPARKLINE-->
+   <script src="vendor/sparklines/jquery.sparkline.min.js"></script>
+   <!-- FLOT CHART-->
+   <script src="../vendor/Flot/jquery.flot.js"></script>
+   <script src="../vendor/flot.tooltip/js/jquery.flot.tooltip.min.js"></script>
+   <script src="../vendor/Flot/jquery.flot.resize.js"></script>
+   <script src="../vendor/Flot/jquery.flot.pie.js"></script>
+   <script src="../vendor/Flot/jquery.flot.time.js"></script>
+   <script src="../vendor/Flot/jquery.flot.categories.js"></script>
+   <script src="../vendor/flot-spline/js/jquery.flot.spline.min.js"></script>
+   <!-- CLASSY LOADER-->
+   <script src="../vendor/jquery-classyloader/js/jquery.classyloader.min.js"></script>
+   <!-- MOMENT JS-->
+   <script src="../vendor/moment/min/moment-with-locales.min.js"></script>
+   <!-- SKYCONS-->
+   <script src="../vendor/skycons/skycons.js"></script>
+   <!-- DEMO-->
+   <script src="js/demo/demo-flot.js"></script>
+   <?PHP require_once(dirname(__FILE__)."/../include/footer-script.tpl.php"); ?>
+   <!-- =============== APP SCRIPTS ===============-->
+   <script src="js/app.js"></script>
+</body>
+
+</html>
