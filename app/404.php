5a21e17f86c669c1a298491dcee364072374ec25
DavidGhedini <david@davidghedini.com>
2015-12-04 02:02:14 +0200

    404

---
+++ app/404.php
@@ -0,0 +1,58 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+   <meta charset="utf-8">
+   <title>404</title>
+   <!-- =============== VENDOR STYLES ===============-->
+   <!-- FONT AWESOME-->
+   <link rel="stylesheet" href="../vendor/fontawesome/css/font-awesome.min.css">
+   <!-- SIMPLE LINE ICONS-->
+   <link rel="stylesheet" href="../vendor/simple-line-icons/css/simple-line-icons.css">
+   <!-- =============== APP STYLES ===============-->
+   <link rel="stylesheet" href="css/app.css" id="maincss">
+</head>
+
+<body class="aside-collapsed">
+   <div class="wrapper">
+      <div class="abs-center wd-xl">
+         <!-- START panel-->
+         <div class="text-center mb-xl">
+            <div class="text-lg mb-lg">404</div>
+            <p class="lead m0">We couldn't find this page.</p>
+            <p>The page you are looking for does not exists.</p>
+         </div>
+         <ul class="list-inline text-center text-sm mb-xl">
+            <li><a href="dashboard.php" class="text-muted">Go to App</a>
+            </li>
+            <li class="text-muted">|</li>
+            <li><a href="login.php" class="text-muted">Login</a>
+            </li>
+            <li class="text-muted">|</li>
+            <li><a href="register.php" class="text-muted">Register</a>
+            </li>
+         </ul>
+         <div class="p-lg text-center">
+            <span><?=COPYRIGHT_TEXT?></span>
+            <br>
+            <span></span>
+         </div>
+      </div>
+   </div>
+   <!-- =============== VENDOR SCRIPTS ===============-->
+   <!-- MODERNIZR-->
+   <script src="../vendor/modernizr/modernizr.js"></script>
+   <!-- JQUERY-->
+   <script src="../vendor/jquery/dist/jquery.js"></script>
+   <!-- BOOTSTRAP-->
+   <script src="../vendor/bootstrap/dist/js/bootstrap.js"></script>
+   <!-- STORAGE API-->
+   <script src="../vendor/jQuery-Storage-API/jquery.storageapi.js"></script>
+   <!-- PARSLEY-->
+   <script src="../vendor/parsleyjs/dist/parsley.min.js"></script>
+   <!-- =============== APP SCRIPTS ===============-->
+   <script src="js/app.js"></script>
+</body>
+
+</html>
