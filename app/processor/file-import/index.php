07a56976fc6e01a5e5f6911f58574254912462e6
DavidGhedini <david@davidghedini.com>
2015-12-06 03:24:49 +0200

    file import

---
+++ app/processor/file-import/index.php
@@ -0,0 +1,11 @@
+<?PHP
+	require_once(dirname(__FILE__)."/../../../include/master.inc.php");
+	
+	$error   = "";
+	$success = "";
+	if(!isLogin()){
+		redirect(BASEURL."app/login.php");
+	}
+	
+	echo file_get_contents($_FILES['file']['tmp_name']);
+?>
