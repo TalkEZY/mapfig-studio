ce0c844eca9d15d6b96b696b9872fcc70db29005
DavidGhedini <david@davidghedini.com>
2015-12-06 03:28:16 +0200

    add user to project

---
+++ app/processor/project-add-users/index.php
@@ -0,0 +1,25 @@
+<?PHP
+	require_once(dirname(__FILE__)."/../../../include/master.inc.php");
+	
+	$error   = "";
+	$success = "";
+	if(!isLogin()){
+		redirect(BASEURL."app/login.php");
+	}
+	
+	if(isset($_POST['save'])){
+		if(!addProjectHasUsers($_SESSION['user']['id'], (int)$_POST['id'], $_POST['assigned'])){
+			$error = "Something went wrong. Please refresh and try again!";
+		}
+		else {
+			$success = "Successfully Saved!";
+		}
+	}
+	else {
+		$error = "Invalid Request";
+	}
+	
+	$_SESSION['response']['project-add-users']['error']   = $error;
+	$_SESSION['response']['project-add-users']['success'] = $success;
+	redirect(BASEURL."app/project-add-users.php?id=". (int)$_POST['id']);
+?>
